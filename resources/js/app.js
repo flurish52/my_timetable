78df20608703d0815c6e4207fd93d58947adb53a
Atuo Israel <atuoisrael3@gmail.com>
2026-04-10 13:40:58 +0100

    testing PWA installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
+
 
 createApp(App).use(router).mount('#app')
+registerSW({ immediate: true })
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
@@ -11,6 +12,35 @@ export default defineConfig({
         }),
         tailwindcss(),
         vue(),
+
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico'],
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: 'Timetable',
+                short_name: 'Timetable',
+                start_url: '/',
+                scope: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#2563EB',
+                icons: [
+                    {
+                        src: '/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
     ],
     server: {
         watch: {
