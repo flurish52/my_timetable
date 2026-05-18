e694b90b25a6cb67b97791a4b327edb4a2e2dc7f
Atuo Israel <atuoisrael3@gmail.com>
2026-05-18 16:00:01 +0100

    correct fce name in past questions page, modify login/register ui, update the edit page ui, add profile and modify profile dropdown and add items, update home button text to timetable

--- public/build/assets/Home-BTgMQfcx.js
+++
@@ -1,1 +0,0 @@
-import{o as e,g as o,a as t}from"./app-B_3cGy1-.js";import{_ as a}from"./Dashboardhome-DQFMI7A0.js";/* empty css            */import"./_plugin-vue_export-helper-DlAUqK2U.js";const r={class:"md:p-4"},d={__name:"Home",setup(s){return(_,c)=>(e(),o("div",r,[t(a)]))}};export{d as default};
---
+++ public/build/assets/Home-DMEFVgcj.js
@@ -0,0 +1,1 @@
+import{o as e,g as o,a as t}from"./app-DQf5o4nG.js";import{_ as a}from"./Dashboardhome-CnPFp6J3.js";/* empty css            */import"./_plugin-vue_export-helper-DlAUqK2U.js";const r={class:"md:p-4"},d={__name:"Home",setup(s){return(_,c)=>(e(),o("div",r,[t(a)]))}};export{d as default};
